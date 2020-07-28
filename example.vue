e3da295e624c65119a36ba9e067f3f57b70f120f
Vivekanand P V <vivekanandpv@outlook.com>
2020-07-28 11:19:16 +0530

    initial commit

---
+++ example.vue
@@ -0,0 +1,19 @@
+<template>
+  <h3>Welcome {{name}}!!!</h3>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      name: "Friends",
+    };
+  },
+};
+</script>
+
+<style>
+h3 {
+  color: blue;
+}
+</style>
